707c2fa7b38dcb1835bcb3a61cc5c44cb979a2d7
arifbudiman17 <arifbudiman_siip@yahoo.co.id>
2016-09-02 21:34:58 +0700

    versi 1.0.3

---
+++ Praktikum_2.3.ino
@@ -0,0 +1,15 @@
+/**************************************************************
+                 ARIF BUDIMAN (4211501009)
+                      MEKATRONIKA-3A
+           PRAKTIKUM 2.3 FUNGSI DELAY PADA BUZZER
+***************************************************************/
+void setup (){
+  pinMode(12, OUTPUT);
+}
+void loop(){
+  digitalWrite(12,HIGH);
+  delay(1000);
+  digitalWrite(12,LOW);
+  delay(1000);
+}
+
