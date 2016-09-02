ac8f320e8277e5bf178b3a3c4c0df601a151c472
arifbudiman17 <arifbudiman_siip@yahoo.co.id>
2016-09-02 21:35:23 +0700

    versi 1.0.4

---
+++ Praktikum_2.4.ino
@@ -0,0 +1,19 @@
+/**************************************************************
+                 ARIF BUDIMAN (4211501009)
+                      MEKATRONIKA-3A
+    PRAKTIKUM 2.4 FUNGSI DELAY PADA BUZZER WITH SWITCH
+***************************************************************/
+void setup(){
+  pinMode(A0, INPUT);
+  digitalWrite(A0, HIGH);
+  pinMode(12, OUTPUT);
+}
+void loop(){
+  if(digitalRead(A0)==LOW){
+    digitalWrite(12, 1);
+  }
+  else{
+    digitalWrite(12, LOW);
+  }
+}
+
