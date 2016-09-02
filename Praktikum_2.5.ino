9c3d687975f6957733761784cbf75c5d0112b2ce
arifbudiman17 <arifbudiman_siip@yahoo.co.id>
2016-09-02 21:35:50 +0700

    versi 1.0.5

---
+++ Praktikum_2.5.ino
@@ -0,0 +1,21 @@
+/**************************************************************
+                 ARIF BUDIMAN (4211501009)
+                      MEKATRONIKA-3A
+    PRAKTIKUM 2.5 FUNGSI DELAY PADA BUZZER WITH SWITCH
+                   KONFIGURASI REGISTER
+***************************************************************/
+void setup(){
+  DDRC=0x00;
+  PORTC=0x01;
+  DDRB=0x10;
+}
+
+void loop(){
+  if(bit_is_clear(PINC,0)){
+    PORTB=0x10;
+  }
+  else{
+    PORTB=0x00;
+  }
+}
+
