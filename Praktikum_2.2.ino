299bb79e2dfdf028cd90f003ee077239071cffd1
arifbudiman17 <arifbudiman_siip@yahoo.co.id>
2016-09-02 21:34:29 +0700

    versi 1.0.2

---
+++ Praktikum_2.2.ino
@@ -0,0 +1,13 @@
+/**************************************************************
+                 ARIF BUDIMAN (4211501009)
+                      MEKATRONIKA-3A
+    PRAKTIKUM 2.2 PEMROGRAMAN DIGITAL OUTPUT MENGGUNAKAN
+                   KONFIGURASI REGISTER
+***************************************************************/
+void setup() {
+  DDRB=0x3F;
+}
+void loop(){
+  PORTB=0x07;
+}
+
