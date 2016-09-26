101f21e8508d19ea5fa651ec3328db654640934e
arifbudiman17 <arifbudiman_siip@yahoo.co.id>
2016-09-26 20:08:49 +0700

    Versi 1.0.7
    Praktikum Mikrokontroller 3.2 (4211501009)_LCD_Analog Input dengan Tegangan Referensi VCC (1,1 Volt)

---
+++ Praktikum_3.2.ino
@@ -0,0 +1,26 @@
+/****************************************************************
+                  ARIF BUDIMAN(4211501009)
+                      MEKATRONIKA 3A
+                  POLITEKNIK NEGERI BATAM
+        PRAKTIKUM 3.2 Analog Input dengan Tegangan   
+                  Referensi VCC (1,1 Volt)
+****************************************************************/
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
+ 
+void setup() { 
+  lcd.begin(16,2);  
+  lcd.clear(); 
+  lcd.setCursor(0,0); 
+  lcd.print("ADC Example on A0");
+  analogReference(DEFAULT); 
+  } 
+ 
+void loop() {  
+  unsigned int dataADC = analogRead(A0);
+  lcd.clear(); 
+  lcd.setCursor(0,1);  
+  lcd.print("Value = ");
+  lcd.print(dataADC); 
+  delay(500);
+} 
