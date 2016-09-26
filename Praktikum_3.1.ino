6b7ed84c8bc0ee8d48196bd2e82a79641ffbbfde
arifbudiman17 <arifbudiman_siip@yahoo.co.id>
2016-09-26 20:06:37 +0700

    Versi 1.0.6
    Praktikum Mikrokontroller 3.1 4211501009

---
+++ Praktikum_3.1.ino
@@ -0,0 +1,26 @@
+/****************************************************************
+                  ARIF BUDIMAN(4211501009)
+                      MEKATRONIKA 3A
+                  POLITEKNIK NEGERI BATAM
+        PRAKTIKUM 3.1 Analog Input dengan Tegangan 
+               Referensi Internal (1,1 Volt)
+****************************************************************/
+#include <LiquidCrystal.h> 
+LiquidCrystal lcd(2,3,4,5,6,7);
+ 
+void setup() {  
+  lcd.begin(16,2); 
+  lcd.clear();   
+  lcd.setCursor(0,0); 
+  lcd.print("ADC Example on A0");
+  analogReference(INTERNAL);
+  } 
+ 
+void loop() {  
+  unsigned int dataADC = analogRead(A0);  
+  lcd.clear();
+  lcd.setCursor(0,1); 
+  lcd.print("Value = " );
+  lcd.print(dataADC); 
+  delay(500); 
+} 
