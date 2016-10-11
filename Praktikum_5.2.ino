73e578f277fe3abe558cd129d903846d1190a11b
Arif Budiman <arifbudiman_siip@yahoo.co.id>
2016-10-11 09:43:25 +0700

    Versi 1.1.4
    Praktikum_Mikrokontroller_5.2_4211501009-Mengirim Data Byte

---
+++ Praktikum_5.2.ino
@@ -0,0 +1,17 @@
+/****************************************************************
+                  ARIF BUDIMAN(4211501009)
+                      MEKATRONIKA 3A
+                  POLITEKNIK NEGERI BATAM
+              PRAKTIKUM 5.2 Mengirim Data Byte
+****************************************************************/
+void setup() {
+Serial.begin(115200);
+}
+void loop() {
+Serial.write(0x00);
+Serial.write(0x01);
+Serial.write(0xF0);
+Serial.write(0x1F);
+Serial.write(0x2F);
+delay(2000);
+}
