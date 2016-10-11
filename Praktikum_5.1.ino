dbea46af45362433df2310e1d0543a42094e4174
Arif Budiman <arifbudiman_siip@yahoo.co.id>
2016-10-11 09:41:48 +0700

    Versi 1.1.3
    Praktikum_Mikrokontroller_5.1_(4211501009) - Mengirim Data Teks

---
+++ Praktikum_5.1.ino
@@ -0,0 +1,13 @@
+/****************************************************************
+                  ARIF BUDIMAN(4211501009)
+                      MEKATRONIKA 3A
+                  POLITEKNIK NEGERI BATAM
+              PRAKTIKUM 5.1 Mengirim Data Teks
+****************************************************************/
+void setup() {
+Serial.begin(9600);
+}
+void loop() {
+Serial.println("Arif Budiman (4211501009)\l");
+delay(1000);
+}
