1a7365574bb93b310a592ea717bd6261e231ce7e
Arif Budiman <arifbudiman_siip@yahoo.co.id>
2016-12-13 04:02:03 -0800

    Versi 1.2.6
    ARIF_BUDIMAN_4211501009_PROJECT_SOUND_GENERATOR_MIKROKONTROLLER

---
+++ Project_Sound_Generator.ino
@@ -0,0 +1,56 @@
+/***************************************************************
+                   ARIF BUDIMAN (4211501009)
+                        MEKATRONIKA 3A
+                   POLITEKNIK NEGERI BATAM
+ **************************************************************/
+// 34 5 5 3 4 5  3 4 4 5 3 4
+// 5 7 7 7 1. 1. 7  7 6 6 4 4 5 3 1.
+
+int speaker =7;
+void setup() {}
+void loop() {
+tone(speaker,349 ); delay(600); //SU
+tone(speaker,395 ); delay(600); //WE
+tone(speaker,395 ); delay(400); //O
+tone(speaker,330 ); delay(600); //RA
+tone(speaker,349 ); delay(400); //JA
+tone(speaker,395 ); delay(700); //MU
+
+tone(speaker,330 ); delay(600); //JA
+tone(speaker,349 ); delay(600); //MU
+tone(speaker,349 ); delay(400); //GO
+tone(speaker,395 ); delay(600); //DONG
+tone(speaker,330 ); delay(400); //TE
+tone(speaker,349 ); delay(700); //LO
+
+tone(speaker,395 ); delay(600); //SU
+tone(speaker,494 ); delay(600); //WE
+tone(speaker,494 ); delay(300); //O
+tone(speaker,494 ); delay(400); //RA
+tone(speaker,520 ); delay(300); //KE
+tone(speaker,520 ); delay(400); //TE
+tone(speaker,494 ); delay(500); //MU
+
+
+tone(speaker,494 ); delay(300); //KE
+tone(speaker,440 ); delay(400); //TE
+tone(speaker,440 ); delay(300); //MU
+tone(speaker,349 ); delay(300); //PI
+tone(speaker,349 ); delay(300); //SAN
+tone(speaker,395 ); delay(500); //GA
+tone(speaker,330 ); delay(600); //WE
+tone(speaker,520 ); delay(700); //LO
+
+/*
+tone(speaker,262 ); delay(500); =DO
+tone(speaker,294 ); delay(500); =RE
+tone(speaker,330 ); delay(500); =MI
+tone(speaker,349 ); delay(500); =FA
+tone(speaker,395 ); delay(500); =SOL
+tone(speaker,440 ); delay(500); =LA
+tone(speaker,494 ); delay(500); =SI
+tone(speaker,520 ); delay(500); =DO
+*/
+}
+
+
