74d7509c0aa7635c86933f19a7b4fabf142b9713
Arif Budiman <arifbudiman_siip@yahoo.co.id>
2017-02-22 14:37:24 +0700

    Versi 1.2.1
    ARIF_BUDIMAN_4211501009_Percobaan_2.2_Dasar_Pergerakkan_Robot

---
+++ Percobaan_2.2_Dasar_Pergerakkan_Robot.ino
@@ -0,0 +1,17 @@
+boolean M1=8; //DIR
+int E1=9; //PWM
+boolean M2=11; //DIR
+int E2=10; //PWM
+void setup(){
+TCCR1B=TCCR1B&0xf8|0x01; // Pin9,Pin10 PWM 31250Hz
+pinMode(M1, OUTPUT);
+pinMode(E1, OUTPUT);
+pinMode(M2, OUTPUT);
+pinMode(E2, OUTPUT);
+}
+void loop(){
+digitalWrite(M1,1);
+analogWrite(E1,30);
+digitalWrite(M2,0);
+analogWrite(E2,30);
+}
